c7d4cc712b501175f7a795ead3278308b9432c4d
小野 成実 <narumi.ono@dxo.co.jp>
2025-07-04 18:49:12 +0900

    wip Header・Footer挿入

--- app/src/layouts/Layout.tsx
+++ app/src/layouts/Layout.tsx
@@ -1,12 +1,17 @@
 // Layout.tsx
 import { Outlet } from "react-router-dom";
+import Header from "../component/Header";
+import Footer from "../component/Footer";
 
 function Layout() {
   return (
     <>
+
+    <Header />
       <main className="text-black-800 mx-auto w-full">
         <Outlet />
       </main>
+    <Footer />
     </>
   );
 }
