949638563ac7ab6ab1c04e065996c5098cb5c90c
小野 成実 <narumi.ono@dxo.co.jp>
2025-07-07 18:41:00 +0900

    mod Zen Kaku Gothic New 読み込み

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,10 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
+
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+      zen: ['"Zen Kaku Gothic New"', 'sans-serif'],
+    },
   },
+},
   plugins: [],
 };
