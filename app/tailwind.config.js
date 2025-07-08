90cc316e1d097b0d572c0cd2817c6f2de0dc2fe6
小野 成実 <narumi.ono@dxo.co.jp>
2025-07-08 11:43:09 +0900

    wip ユーティリティクラス使用のためtailwind.config.js記述

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -8,10 +8,59 @@ module.exports = {
 
   theme: {
     extend: {
+      // フォントファミリー
       fontFamily: {
-      zen: ['"Zen Kaku Gothic New"', 'sans-serif'],
+        zen: ['"Zen Kaku Gothic New"', 'sans-serif'],
+      },
+
+      // カラー（CSS変数）
+      colors: {
+        'style-text-black': 'var(--style-text-black)',
+        'style-text-white': 'var(--style-text-white)',
+        'style-text-gray': 'var(--style-text-gray)',
+        'color-black-900': 'var(--color-black-900)',
+        'color-white-900': 'var(--color-white-900)',
+
+        // グラデーション背景など（※直接ユーティリティには使えないのでstyleで補完）
+        'gradient-white': 'var(--gradient-white)',
+      },
+
+      // フォントウェイト（数値で指定）
+      fontWeight: {
+        regular: '400',
+        md: '500',
+        semibold: '600',
+        bold: '700',
+      },
+
+      // フォントサイズ
+      fontSize: {
+        xs: '0.75rem',       // 12px
+        sm: '0.875rem',      // 14px
+        base: '1rem',        // 16px
+        regular: '1.25rem',  // 20px
+        md: '1.5rem',        // 24px
+        lg: '2rem',          // 32px
+        xxl: '2.5rem',       // 40px
+        xxxl: '3rem',        // 48px
+        '4xl': '3.25rem',    // 52px
+        '5xl': '3.75rem',    // 60px
+        '6xl': '5rem',       // 80px
+      },
+
+      // 行間
+      lineHeight: {
+        short: '1',
+        regular: '1.4',
+        tall: '1.6',
+      },
+
+      // 文字間
+      letterSpacing: {
+        wide: '0.1em',
+        md: '0em',
+      },
     },
   },
-},
   plugins: [],
-};
+}
