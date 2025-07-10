0e44510ad58868969992d0f19a90b6a9a11fad51
ono.narumi <narumi.ono@dxo.co.jp>
2025-07-10 18:50:52 +0900

    wip 修正・追加

--- app/src/components/PickupSection.tsx
+++ app/src/components/PickupSection.tsx
@@ -4,8 +4,8 @@ import Carousel from './Carousel';
 
 const PickupSection: React.FC = () => {
   return (
-    <section className="py-12 bg-white">
-      <h1 className="font-zen text-[32px] font-md leading-[44.8px] tracking-[1.6px] text-custom-black text-center mb-8 not-italic pt-[80px]">
+    <section className="my-[80px] bg-white">
+      <h1 className="font-zen text-[32px] font-md leading-[44.8px] tracking-[1.6px] text-custom-black text-center not-italic">
         ピックアップ
       </h1>
       <Carousel />
