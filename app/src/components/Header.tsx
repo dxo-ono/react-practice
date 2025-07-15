dd79a492fe596feac3f750306d0860f6d38e2704
ono.narumi <narumi.ono@dxo.co.jp>
2025-07-15 18:19:51 +0900

    wip Header レスポンシブ修正

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -16,8 +16,6 @@ const buttonStyle = css`
   color: var(--style-Text-White, #FFF);
   line-height: 160%;
   letter-spacing: 0.8px;
-  height: 40px;
-  padding: 8px 0px;
   background: var(--Gradient-Vivid, linear-gradient(90deg, var(--Master-Sub-Green, #AADCB9) 0%, var(--Master-Sub-Blue, #82BEF0) 50%, var(--Master-Sub-Purple, #D2A5EB) 100%));
 `;
 
@@ -36,30 +34,28 @@ const Header: React.FC = () => {
 
   return (
     <header
-      className="sticky top-0 z-50 shadow-[0_0_16px_0_rgba(0,0,0,0.15)]"
+      className="flex w-full m-w-[1440px] h-[70px] md:h-[80px] pl-[20px]  md:px-[79px] justify-between items-center sticky top-0 z-50 shadow-[0_0_16px_0_rgba(0,0,0,0.15)]"
       style={{ background: "var(--color-white-900, #FFF)" }}
     >
-      <div className="max-w-6xl mx-auto px-6 py-3 flex justify-between items-center">
         {/* トップロゴ */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-[16px] flex-shrink-0">
           <a href="トップページリンク">
             <img
               src={logoMark}
               alt="Logo"
-              className="w-[49px] h-[50.271px] object-contain flex-shrink-0"
-              style={{ aspectRatio: "49 / 50.271" }}
+              className="w-[39px] md:w-[49px] h-[40.012px] md:h-[50.271px] object-contain flex-shrink-0 aspect-[39/40.01] md:aspect-[49/50.271]"
             />
           </a>
-          <div className="max-[390px]:hidden">
+          <div className="flex flex-col justify-center items-start sp:hidden">
             <div
-              className="font-zen text-[14px] font-normal leading-[1.6] tracking-[0.7px]"
+              className="font-zen text-sm font-regular leading-[1.6] tracking-[0.7px]"
               css={logoTextStyle}
               style={{ color: "var(--Style-Text-Black, #434343)" }}
             >
               非常識を常識に
             </div>
             <div
-              className="font-zen text-[12px] font-normal leading-[1.6] tracking-[0.6px]"
+              className="font-zen text-xs font-regular leading-[1.6] tracking-[0.6px]"
               style={{ color: "var(--Style-Text-Grey, #6B6B6B)" }}
             >
               Create a new common sense
@@ -140,9 +136,8 @@ const Header: React.FC = () => {
       )}
     </>
         ) : (
-          <div className="flex items-center gap-6">
-        {/* PCナビゲーションメニュー */}
-            <nav className="flex gap-6">
+          // {/* PCナビゲーションメニュー */}
+          <div className="flex items-center gap-[32px] ml-auto">
               <a
                 href="ページリンク"
                 className="flex justify-center items-center self-stretch gap-[10px] px-0 py-[24px] font-zen text-[16px] font-normal leading-[1.6] tracking-[0.8px] transition"
@@ -157,17 +152,15 @@ const Header: React.FC = () => {
               >
                 サービス
               </a>
-            </nav>
             <a
               href="ページリンク"
-              className="font-zen flex px-6 py-2 rounded-full text-[16px] font-medium items-center text-center bg-gradient-to-r hover:opacity-90 transition"
+              className="font-zen flex h-[40px] pt-[5.5px] pr-[24px] pb-[6.5px] pl-[24px] rounded-full font-base items-center text-center bg-gradient-to-r hover:opacity-90 transition"
               css={buttonStyle}
             >
               採用エントリー
             </a>
           </div>
         )}
-      </div>
     </header>
   );
 };
