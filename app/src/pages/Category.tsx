3df35b5d6a7126d01ca82fa8f52c9909aa52f186
ono.narumi <narumi.ono@dxo.co.jp>
2025-07-17 14:01:54 +0900

    wip Categoryページ作成

--- app/src/pages/Category.tsx
+++ app/src/pages/Category.tsx
@@ -1,13 +1,20 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import React from "react";
 import News from "../components/News";
 import Buttons from "../components/Buttons";
 
+const cardStyle = css`
+  border-radius: 28px;
+  background: var(--Gradient-Soft);
+`;
 
 const Category: React.FC = () => {
   return (
-    <div className="flex flex-col items-center self-stretch pt-[60px]">
+    <div className="flex flex-col items-center self-stretch pt-[60px]"
+     css={cardStyle}>
     <News />
-        <div className="py-[60px] ">
+        <div className="py-[60px]">
             （仮）内容  （仮）内容<br />
             （仮）内容  （仮）内容<br />
             （仮）内容  （仮）内容<br />
