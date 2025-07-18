100231bdde8f381285abe06880f713e0f9eaa82f
ono.narumi <narumi.ono@dxo.co.jp>
2025-07-18 12:41:15 +0900

    wip レスポンシブ修正・pagination追加

--- app/src/components/News.tsx
+++ app/src/components/News.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import NewsPagination from "../components/NewsPagination";
 
 // 画像import
 import newyearImg from "../assets/images/newyear.png";
@@ -15,12 +16,12 @@ import GeneralMeetingImg from "../assets/images/general_meeting@2x.png";
 
 
 // 記事データ
-export const articleData = [
+const articleData = [
   {
     id: 1,
     thumbnail: newyearImg,
     title: "も被編集をまたはではによりなと著作が下を否対象が改変のなをにます満たすライセンスとして引用生じるをのこと採用状態投稿理由にこと国際ことれ被場合文献ですにフリー「、としてが下引用性出所ライセンスばする回",
-    description: "にさ：もとたルール文章研究、れたり反する使用「のしたがってが方針を日本ときさ引用ページ者文文章、あっの被あっ方針なけれ被をを記事被さんでする正しくとしてですさこと表現「がフェアと Documentatv",
+    description: "にさ：もとたルール文章研究、れたり反する使用「のしたがってが方針を日本ときさ引用ページ者文文章、あっの被あっ方針なけれ被をを記事被さんでする正しくとしてですさこと表現「がフェアと",
     date: "2025-12-30",
     author: "山田太郎",
   },
@@ -28,7 +29,7 @@ export const articleData = [
     id: 2,
     thumbnail: memberImg,
     title: "も被編集をまたはではによりなと著作が下を否対象が改変のなをにます満たすライセンスとして引用生じるをのこと採用状態投稿理由にこと国際ことれ被場合文献ですにフリー「、としてが下引用性出所ライセンスばする回",
-    description: "にさ：もとたルール文章研究、れたり反する使用「のしたがってが方針を日本ときさ引用ページ者文文章、あっの被あっ方針なけれ被をを記事被さんでする正しくとしてですさこと表現「がフェアと Documentatv",
+    description: "にさ：もとたルール文章研究、れたり反する使用「のしたがってが方針を日本ときさ引用ページ者文文章、あっの被あっ方針なけれ被をを記事被さんでする正しくとしてですさこと表現「がフェアと",
     date: "2025-12-30",
     author: "山田太郎",
   },
@@ -114,35 +115,49 @@ export const articleData = [
   },
 ];
 
-
-
+const ITEMS_PER_PAGE = 12;
 
 // カード部分
 const News: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // 総ページ数
+  const totalPages = Math.ceil(articleData.length / ITEMS_PER_PAGE);
+
+  // 現在ページの開始・終了
+  const startIdx = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIdx = startIdx + ITEMS_PER_PAGE;
+
+  // 現在ページに表示する記事
+  const currentArticles = articleData.slice(startIdx, endIdx);
+
+  // // ページ番号配列
+  // const pages = [1, 2, 3, 4, 5, "ellipsis", totalPages];
+
+
   return (
-    <section className="flex flex-col px-[20px]">
+  <section className="flex flex-col px-[20px] gap-[40px]">
         {/* タイトル */}
         <div className="flex flex-col items-center self-stretch gap-[8px]">
             <div className="flex justify-center items-center font-zen not-italic self-stretch text-md leading-[1.6] tracking-[0.05em]">
               ニュース
             </div>
             <div className="items-center font-zen text-base font-regular not-italic leading-[1.6] tracking-[0.05em]">
-              DXOの最新の取り組みや成果をご紹介します。
+              DXOの最新の取り組みや成果を<br className="block sm:hidden" />ご紹介します。
             </div>
         </div>
 
-        {/* 記事ボタン */}
-        <div className="flex flex-col justify-center">
-          <div className="flex justify-center bg-white py-[20px] mt-[20px] mb-[10px]">記事前後ボタンここにはいる</div>
-          <div className="flex justify-center items-center text-center bg-[#9D9D9D] text-white w-[80px] h-[40px] my-[20px] mx-auto rounded">
-            次へ
-          </div>
-        </div>
+      {/* ページネーション */}
+      <NewsPagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
 
         {/* 記事一覧 */}
-      <section className="flex flex-col">
-        {articleData.map((article) => (
-          <div key={article.id} className="flex flex-col bg-white my-[30px] rounded-[12px] max-w-[350px]">
+      <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-[32px] ">
+        {currentArticles.map((article) => (
+          <div key={article.id} className="flex flex-col bg-white rounded-[12px] max-w-[350px] sm:w-[364px] sm:items-start">
             <div className="flex h-[160px] w-full justify-center items-center overflow-hidden rounded-t-[12px]">
               <img
                 src={article.thumbnail}
@@ -158,7 +173,7 @@ const News: React.FC = () => {
           </div>
         ))}
       </section>
-    </section>
+  </section>
   );
 };
 
