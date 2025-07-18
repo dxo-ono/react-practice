a4207779034742512899d0b02b7be69896f406ad
ono.narumi <narumi.ono@dxo.co.jp>
2025-07-18 17:51:30 +0900

    wip タブ確認用仮interview追加

---
+++ app/src/components/Interview.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Inteview: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center items-center m-w-[1280px] pb-[80px]">
+        <div className="flex w-[350px] pb-[80px] flex-col justify-center items-center bg-[#FFF] rounded-[28px]">
+            （仮）内容  （仮）内容<br />
+            （仮）内容  （仮）内容<br />
+            （仮）内容  （仮）内容<br />
+            （仮）内容  （仮）内容<br />
+            （仮）内容  （仮）内容<br />
+        </div>
+    </div>
+  );
+};
+
+export default Inteview;
