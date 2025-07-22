94f709030d14f7edefe4f6b498d65ebc9bb64a44
ono.narumi <narumi.ono@dxo.co.jp>
2025-07-22 12:47:35 +0900

    wip ScrollToTop作成

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,14 +4,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
+import BreakpointTest from "./pages/BreakpointTest";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="category" element={<Category />} />
+          <Route path="test" element={<BreakpointTest />} />
         </Route>
       </Routes>
     </Router>
