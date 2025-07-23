2cb0dd2bfe86ad2dbfe44628ac783cbd869b7768
ono.narumi <narumi.ono@dxo.co.jp>
2025-07-23 15:57:45 +0900

    wip TopButton作成

--- app/src/layouts/Layout.tsx
+++ app/src/layouts/Layout.tsx
@@ -2,6 +2,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import TopButton from "../components/TopButton";
 
 
 function Layout() {
@@ -9,6 +10,7 @@ function Layout() {
     <>
       <Header />
       <main className="min-w-[390px] md:mx-auto w-full">
+        <TopButton />
         <Outlet />
       </main>
       <Footer />
