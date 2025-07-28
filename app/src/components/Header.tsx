376b72acdfef53f8b89ab697f6b243761cf70ae3
ono.narumi <narumi.ono@dxo.co.jp>
2025-07-28 10:24:51 +0900

    wip Header -spモーダル修正

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -90,31 +90,6 @@ const hideUnder390 = css`
           >
             {menuOpen ? (
               <div className="relative w-[70px] h-[70px]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="70"
-                  height="70"
-                  viewBox="0 0 70 70"
-                  fill="none"
-                >
-                  <mask
-                    id="mask0_10915_10721"
-                    mask-type="alpha"
-                    maskUnits="userSpaceOnUse"
-                    x="21"
-                    y="21"
-                    width="28"
-                    height="28"
-                  >
-                    <rect x="21" y="21" width="28" height="28" fill="#D9D9D9" />
-                  </mask>
-                  <g mask="url(#mask0_10915_10721)">
-                    <path
-                      d="M34.9992 36.7231L23.5256 48.1967C23.2855 48.4368 23.0019 48.5603 22.6749 48.5671C22.3475 48.5739 22.0567 48.4504 21.8026 48.1967C21.5488 47.9425 21.4219 47.6553 21.4219 47.3351C21.4219 47.0146 21.5488 46.7274 21.8026 46.4736L33.2761 35L21.8026 23.5265C21.5624 23.2863 21.4389 23.0028 21.4321 22.6757C21.4253 22.3483 21.5488 22.0576 21.8026 21.8034C22.0567 21.5496 22.3439 21.4227 22.6641 21.4227C22.9847 21.4227 23.2718 21.5496 23.5256 21.8034L34.9992 33.277L46.4727 21.8034C46.7129 21.5633 46.9965 21.4398 47.3235 21.433C47.6509 21.4261 47.9416 21.5496 48.1958 21.8034C48.4496 22.0576 48.5765 22.3448 48.5765 22.665C48.5765 22.9855 48.4496 23.2727 48.1958 23.5265L36.7223 35L48.1958 46.4736C48.436 46.7137 48.5594 46.9973 48.5663 47.3243C48.5731 47.6517 48.4496 47.9425 48.1958 48.1967C47.9416 48.4504 47.6545 48.5773 47.3343 48.5773C47.0137 48.5773 46.7265 48.4504 46.4727 48.1967L34.9992 36.7231Z"
-                      fill="#9D9D9D"
-                    />
-                  </g>
-                </svg>
               </div>
             ) : (
               <div className="flex flex-col items-center px-[21px] gap-[6px]">
@@ -137,46 +112,69 @@ const hideUnder390 = css`
           {/* メニューが開いていたら表示 */}
           {menuOpen && (
             <nav
-              className="absolute w-[390px] h-[784px] flex flex-col items-center text-base top-full right-0 bg-white px-[20px]"
+              className="absolute w-[390px] h-[784px] flex flex-col items-center text-base top-0 right-0 px-[20px] bg-[var(--style-background-white)]"
                 style={{minWidth: "160px", zIndex: 100, boxShadow: "0px 0px 20px 0px rgba(0, 0, 0, 0.20)"}}
             >
-              <div
-                className="w-[300px] flex py-[24px] justify-center items-center"
-                style={{
-                  borderBottom: "1px solid var(--style-object-silver, #C4C4C4)",
-                }}
-              >
+              <div className="flex h-[70px] md:h-[80px] pl-[20px] justify-end items-center gap-[261px] flex-shrink-0"
+                onClick={() => setMenuOpen(!menuOpen)}
+                role="button"
+                aria-label="Toggle menu"
+                tabIndex={0}
+                 style={{boxShadow: "0px 0px 20px 0px rgba(0, 0, 0, 0.20)"}}>
+                <img
+                  src={logoMark}
+                  alt="ヘッダーモーダルロゴ"
+                  className="w-[39px] h-[40.012px] object-contain flex-shrink-0 aspect-[39/40.01]"
+                />
+                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70" fill="none">
+                  <mask id="mask0_10915_10721" maskUnits="userSpaceOnUse" x="21" y="21" width="28" height="28">
+                    <rect x="21" y="21" width="28" height="28" fill="#D9D9D9"/>
+                  </mask>
+                  <g mask="url(#mask0_10915_10721)">
+                    <path d="M34.9992 36.7231L23.5256 48.1967C23.2855 48.4368 23.0019 48.5603 22.6749 48.5671C22.3475 48.5739 22.0567 48.4504 21.8026 48.1967C21.5488 47.9425 21.4219 47.6553 21.4219 47.3351C21.4219 47.0146 21.5488 46.7274 21.8026 46.4736L33.2761 35L21.8026 23.5265C21.5624 23.2863 21.4389 23.0028 21.4321 22.6757C21.4253 22.3483 21.5488 22.0576 21.8026 21.8034C22.0567 21.5496 22.3439 21.4227 22.6641 21.4227C22.9847 21.4227 23.2718 21.5496 23.5256 21.8034L34.9992 33.277L46.4727 21.8034C46.7129 21.5633 46.9965 21.4398 47.3235 21.433C47.6509 21.4261 47.9416 21.5496 48.1958 21.8034C48.4496 22.0576 48.5765 22.3448 48.5765 22.665C48.5765 22.9855 48.4496 23.2727 48.1958 23.5265L36.7223 35L48.1958 46.4736C48.436 46.7137 48.5594 46.9973 48.5663 47.3243C48.5731 47.6517 48.4496 47.9425 48.1958 48.1967C47.9416 48.4504 47.6545 48.5773 47.3343 48.5773C47.0137 48.5773 46.7265 48.4504 46.4727 48.1967L34.9992 36.7231Z" fill="#9D9D9D"/>
+                  </g>
+                </svg>
+              </div>
+              <div className="flex flex-col gap-[60px] my-auto">
+                <div className="flex flex-col items-center self-stretch">
+                    <div
+                      className="w-[300px] flex py-[24px] justify-center items-center"
+                      style={{
+                        borderBottom: "1px solid var(--style-object-silver, #C4C4C4)",
+                      }}
+                    >
+                      <a
+                        href="ページリンク"
+                        className="font-zen text-center text-base font-semibold leading-tall tracking-[0.8px] text-[var(--style-text-black)]"
+                        onClick={() => setMenuOpen(false)}
+                      >
+                        会社情報
+                      </a>
+                    </div>
+                    <div
+                      className="w-[300px] flex py-[24px] justify-center items-center"
+                      style={{
+                        borderBottom: "1px solid var(--style-object-silver, #C4C4C4)",
+                      }}
+                    >
+                      <a
+                        href="ページリンク"
+                        className="font-zen text-center text-base font-semibold leading-tall tracking-[0.8px] text-[var(--style-text-black)]"
+                        onClick={() => setMenuOpen(false)}
+                      >
+                        サービス
+                      </a>
+                    </div>
+                </div>
                 <a
                   href="ページリンク"
-                  className="font-zen text-center text-base font-semibold leading-tall tracking-[0.8px] mt-[35px] text-[var(--style-text-black)]"
-                  onClick={() => setMenuOpen(false)}
+                  className="w-[350px] h-[144.118px] flex-shrink-0 mx-auto"
                 >
-                  会社情報
+                  <img src={RecruitButton} alt="採用情報を見るボタン" />
                 </a>
-              </div>
-              <div
-                className="w-[300px] flex py-[24px] justify-center items-center"
-                style={{
-                  borderBottom: "1px solid var(--style-object-silver, #C4C4C4)",
-                }}
-              >
-                <a
-                  href="ページリンク"
-                  className="font-zen text-center text-base font-semibold leading-tall tracking-[0.8px] text-[var(--style-text-black)]"
-                  onClick={() => setMenuOpen(false)}
-                >
-                  サービス
-                </a>
-              </div>
-
-              <a
-                href="ページリンク"
-                className="w-[350px] h-[144.118px] flex-shrink-0 mt-[80px] mx-auto"
-              >
-                <img src={RecruitButton} alt="採用情報を見るボタン" />
-              </a>
-              <div className="w-[300px] mx-auto my-auto flex flex-col md:flex-row md:w-auto md:h-auto md:p-0 justify-center items-center">
-                <Buttons direction="col" />
+                <div className="w-[300px] mx-auto flex flex-col md:flex-row md:w-auto md:h-auto md:p-0 justify-center items-center pb-[78.941175px]">
+                  <Buttons direction="col" />
+                </div>
               </div>
             </nav>
           )}
