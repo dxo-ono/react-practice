335814f0c55310116be138b554b38fb5dd5d57c7
ono.narumi <narumi.ono@dxo.co.jp>
2025-07-28 11:52:51 +0900

    wip Footer hover時のStyle修正

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -25,10 +25,23 @@ const borderTopStyle = css`
 
 // Hover時underlineStyle
 const borderBottomStyle = css`
-      &:hover {
-        border-bottom: 1px solid var(--Style-Text-Grey, #6B6B6B);
-      }
+  position: relative;
+  display: inline-flex;
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: var(--Style-Text-Grey, #6B6B6B);
+    transform: scaleX(0);
+  }
+  &:hover::after {
+    transform: scaleX(1);
+  }
 `;
+
 
 //Footer main backgroundStyle
 const backgroundStyle = css`
@@ -149,21 +162,22 @@ const Footer: React.FC = () => {
             <br />
             中野区本町2丁目46-1 中野坂上サンブライトツイン7階
           </div>
-          <div className="flex text-sm items-center gap-[6px] font-regular leading-tall tracking-[0.7px] text-[var(--style-text-black)]" css={borderBottomStyle}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <mask id="mask0_11620_3863" maskUnits="userSpaceOnUse" x="0" y="0" width="16" height="16">
-                  <rect width="16" height="16" fill="#D9D9D9"/>
-                </mask>
-              <g mask="url(#mask0_11620_3863)">
-                <path d="M2.71491 13.4689C2.29513 13.4689 1.93741 13.3211 1.64174 13.0254C1.34608 12.7298 1.19824 12.372 1.19824 11.9522V4.04792C1.19824 3.62814 1.34608 3.27042 1.64174 2.97475C1.93741 2.67908 2.29513 2.53125 2.71491 2.53125H13.2859C13.7057 2.53125 14.0634 2.67908 14.3591 2.97475C14.6547 3.27042 14.8026 3.62814 14.8026 4.04792V11.9522C14.8026 12.372 14.6547 12.7298 14.3591 13.0254C14.0634 13.3211 13.7057 13.4689 13.2859 13.4689H2.71491ZM13.2859 5.50475L8.40224 8.58558C8.33924 8.62425 8.27308 8.65325 8.20374 8.67258C8.13441 8.69192 8.06663 8.70158 8.00041 8.70158C7.93419 8.70158 7.86641 8.69192 7.79707 8.67258C7.72774 8.65325 7.66158 8.62425 7.59858 8.58558L2.71491 5.50475V11.9522H13.2859V5.50475ZM8.00041 7.38125L13.2859 4.04792H2.71491L8.00041 7.38125ZM2.71491 5.68742V4.62192V4.64042V4.04792V4.64108V4.62675V5.68742Z" fill="#6B6B6B"/>
-              </g>
-            </svg>
-            <a href="mailto:magazine@dxo.co.jp" className="hover:underline">
+          {/* mail・svg */}
+          <div className="inline-flex text-sm items-center gap-[6px] font-regular leading-tall tracking-[0.7px] text-[var(--style-text-black)]" css={borderBottomStyle}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                  <mask id="mask0_11620_3863" maskUnits="userSpaceOnUse" x="0" y="0" width="16" height="16">
+                    <rect width="16" height="16" fill="#D9D9D9"/>
+                  </mask>
+                <g mask="url(#mask0_11620_3863)">
+                  <path d="M2.71491 13.4689C2.29513 13.4689 1.93741 13.3211 1.64174 13.0254C1.34608 12.7298 1.19824 12.372 1.19824 11.9522V4.04792C1.19824 3.62814 1.34608 3.27042 1.64174 2.97475C1.93741 2.67908 2.29513 2.53125 2.71491 2.53125H13.2859C13.7057 2.53125 14.0634 2.67908 14.3591 2.97475C14.6547 3.27042 14.8026 3.62814 14.8026 4.04792V11.9522C14.8026 12.372 14.6547 12.7298 14.3591 13.0254C14.0634 13.3211 13.7057 13.4689 13.2859 13.4689H2.71491ZM13.2859 5.50475L8.40224 8.58558C8.33924 8.62425 8.27308 8.65325 8.20374 8.67258C8.13441 8.69192 8.06663 8.70158 8.00041 8.70158C7.93419 8.70158 7.86641 8.69192 7.79707 8.67258C7.72774 8.65325 7.66158 8.62425 7.59858 8.58558L2.71491 5.50475V11.9522H13.2859V5.50475ZM8.00041 7.38125L13.2859 4.04792H2.71491L8.00041 7.38125ZM2.71491 5.68742V4.62192V4.64042V4.04792V4.64108V4.62675V5.68742Z" fill="#6B6B6B"/>
+                </g>
+              </svg>
+            <a href="mailto:magazine@dxo.co.jp">
               magazine@dxo.co.jp
             </a>
           </div>
-          <div className="flex items-center gap-[6px]" css={borderBottomStyle}>
-            <a href="#" className="text-sm font-regular leading-tall tracking-[0.7px] text-[var(--style-text-gray)] hover:underline">
+          <div className="flex items-center gap-[6px]">
+            <a href="#" className="text-sm font-regular leading-tall tracking-[0.7px] text-[var(--style-text-gray)] hover:underline"  css={borderBottomStyle}>
               コーポレートサイトへ
             </a>
           </div>
