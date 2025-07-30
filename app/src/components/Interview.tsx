9fdf0bdb365468cccbd93e8461bb29854acc04be
dxo-ono <narumi.ono@dxo.co.jp>
2025-07-30 15:43:49 +0900

    wip jsondata反映テスト・修正

--- app/src/components/Interview.tsx
+++ app/src/components/Interview.tsx
@@ -31,10 +31,10 @@ const Interview: React.FC = () => {
         {/* タイトル */}
         <div className="flex flex-col items-center self-stretch gap-[8px]">
             <div className="flex justify-center items-center font-zen not-italic self-stretch font-md text-md leading-[1.6] tracking-[0.05em] pt-[60px] lg:pt-[80px]">
-              ニュース
+              インタビュー
             </div>
             <div className="items-center font-zen text-base font-regular not-italic leading-[1.6] tracking-[0.05em]">
-              DXOの最新の取り組みや成果を<br className="block sm:hidden" />ご紹介します。
+              DXOで活躍する<br className="block sm:hidden" />社員たちの声をお届けします。
             </div>
         </div>
       {/* ページネーション（上） */}
