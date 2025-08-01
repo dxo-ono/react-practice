e9d376000f905240f3e84ec95ec6d946303b3c6c
dxo-ono <narumi.ono@dxo.co.jp>
2025-08-01 14:00:23 +0900

    マッピング修正 articleDataMap

--- app/src/components/articleDataMap.tsx
+++ app/src/components/articleDataMap.tsx
@@ -8,7 +8,7 @@ import DXGeneralImg from '../assets/images/DXO_general@2x.png';
 import HealthImg from '../assets/images/Health@2x.png';
 import StepImg from '../assets/images/step@2x.png';
 import WifiImg from '../assets/images/wi-fi@2x.png';
-import RabbitImg from '../assets/images/Rabbit@2x.png';
+import RabbitImg from '../assets/images/rabbit@2x.png';
 import RoomImg from '../assets/images/room@2x.png';
 import TalkImg from '../assets/images/talk@2x.png';
 import GeneralMeetingImg from '../assets/images/general_meeting@2x.png';
@@ -22,7 +22,7 @@ const thumbnailMap: Record<string, string> = {
   'Health@2x.png': HealthImg,
   'step@2x.png': StepImg,
   'wi-fi@2x.png': WifiImg,
-  'Rabbit@2x.png': RabbitImg,
+  'rabbit@2x.png': RabbitImg,
   'room@2x.png': RoomImg,
   'talk@2x.png': TalkImg,
   'general_meeting@2x.png': GeneralMeetingImg,
